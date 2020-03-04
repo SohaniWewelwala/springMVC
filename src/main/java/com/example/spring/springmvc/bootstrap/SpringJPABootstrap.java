80a275162ecdf03bfaa6e5abec4cb446c9072a12
Sohani <sohaniwewelwala@gmail.com>
2020-03-04 14:59:06 +0530

    Load Data via Spring

---
+++ src/main/java/com/example/spring/springmvc/bootstrap/SpringJPABootstrap.java
@@ -0,0 +1,66 @@
+package com.example.spring.springmvc.bootstrap;
+
+import com.example.spring.springmvc.model.Product;
+import com.example.spring.springmvc.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@Component
+public class SpringJPABootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        loadProducts();
+    }
+
+    private void loadProducts() {
+
+        Product product1 = new Product();
+        product1.setId(1);
+        product1.setDescription("Product 1");
+        product1.setPrice(new BigDecimal("12.99"));
+        product1.setImageUrl("http://example.com/product1");
+        productService.updateProduct(product1);
+
+        Product product2 = new Product();
+        product2.setId(2);
+        product2.setDescription("Product 2");
+        product2.setPrice(new BigDecimal("14.99"));
+        product2.setImageUrl("http://example.com/product2");
+        productService.updateProduct(product2);
+
+        Product product3 = new Product();
+        product3.setId(3);
+        product3.setDescription("Product 3");
+        product3.setPrice(new BigDecimal("34.99"));
+        product3.setImageUrl("http://example.com/product3");
+        productService.updateProduct(product3);
+
+        Product product4 = new Product();
+        product4.setId(4);
+        product4.setDescription("Product 4");
+        product4.setPrice(new BigDecimal("44.99"));
+        product4.setImageUrl("http://example.com/product4");
+        productService.updateProduct(product4);
+
+        Product product5 = new Product();
+        product5.setId(5);
+        product5.setDescription("Product 5");
+        product5.setPrice(new BigDecimal("25.99"));
+        product5.setImageUrl("http://example.com/product5");
+        productService.updateProduct(product5);
+    }
+
+}
